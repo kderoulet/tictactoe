160696d0443814bfc658da109d93be0ca1ca8da8
Kevin de Roulet <kderoulet@yahoo.com>
2017-09-29 15:49:39 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+var score = [0, 0]
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
